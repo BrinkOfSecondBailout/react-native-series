5e06f6682c44edaa2780cee0b892641bc57d9c18
Danny Nguyen <nguyenduongpa@gmail.com>
2024-11-30 12:03:38 -0800

    myapp

---
+++ MyApp/app/explore.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>Generic Explore Information</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
