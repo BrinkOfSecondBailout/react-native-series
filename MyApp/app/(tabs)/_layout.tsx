fc496171699f1672a28a119e855f3d1c15f95d9e
Danny Nguyen <nguyenduongpa@gmail.com>
2024-11-30 16:53:46 -0800

    d

--- MyApp/app/(tabs)/_layout.tsx
+++ MyApp/app/(tabs)/_layout.tsx
@@ -15,7 +15,9 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        // headerTitleAlign: 'center',
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
@@ -30,13 +32,13 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name={"house.fill"} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="contact"
         options={{
-          title: 'Explore',
+          title: 'Contact',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
--- MyApp/app/(tabs)/contact.tsx
+++ MyApp/app/(tabs)/contact.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function TabTwoScreen() {
   return (
     <View>
-      <Text>Generic Explore Information</Text>
+      <Text>Contact Information</Text>
     </View>
   );
 }
