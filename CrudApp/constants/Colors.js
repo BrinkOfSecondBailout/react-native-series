edd58c08961aae8f36bf71e5ddf4c73d0fc32806
Danny Nguyen <nguyenduongpa@gmail.com>
2024-12-05 17:18:05 -0800

    d

--- CrudApp/constants/Colors.js
+++ CrudApp/constants/Colors.js
@@ -1,23 +1,14 @@
-const tintColorLight = 'orange';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    headerBackground: 'rgb(242, 242, 242)',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#c68e17',
-    tabIconSelected: tintColorLight,
+    text: '#black',
+    background: 'white',
+    icon: '#black',
+    button: 'royalblue',
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    headerBackground: 'rgb(1, 1, 1)',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: 'white',
+    background: 'black',
+    icon: 'red',
+    button: 'white',
   },
 };
